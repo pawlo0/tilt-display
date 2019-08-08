0e45d51026e3ca68d7c4e8da93f8211666c48911
pawlo0 <paulo_sss@hotmail.com>
2019-08-08 18:04:32 +0100

    fix issue of last zero

---
+++ display.py
@@ -0,0 +1,102 @@
+import blescan
+import sys
+import time
+from datetime import datetime 
+import bluetooth._bluetooth as bluez
+import ZeroSeg.led as led
+
+device = led.sevensegment(cascaded=2)
+
+
+#Assign uuid's of various colour tilt hydrometers. BLE devices like the tilt work primarily using advertisements. 
+#The first section of any advertisement is the universally unique identifier. Tilt uses a particular identifier based on the colour of the device
+red    	= 'a495bb10c5b14b44b5121370f02d74de'
+green  	= 'a495bb20c5b14b44b5121370f02d74de'
+black  	= 'a495bb30c5b14b44b5121370f02d74de'
+purple 	= 'a495bb40c5b14b44b5121370f02d74de'
+orange 	= 'a495bb50c5b14b44b5121370f02d74de'
+blue   	= 'a495bb60c5b14b44b5121370f02d74de'
+yellow 	= 'a495bb70c5b14b44b5121370f02d74de'
+pink   	= 'a495bb80c5b14b44b5121370f02d74de'
+
+#The default device for bluetooth scan. If you're using a bluetooth dongle you may have to change this.
+dev_id = 0
+
+#scan BLE advertisements until we see one matching our tilt uuid
+def getdata():
+	try:
+		sock = bluez.hci_open_dev(dev_id)
+
+	except:
+		print "error accessing bluetooth device..."
+		sys.exit(1)
+
+	blescan.hci_le_set_scan_parameters(sock)
+	blescan.hci_enable_le_scan(sock)
+
+	gotData = 0
+
+	while (gotData == 0):
+
+		returnedList = blescan.parse_events(sock, 10)
+
+		for beacon in returnedList: #returnedList is a list datatype of string datatypes seperated by commas (,)
+			output = beacon.split(',') #split the list into individual strings in an array
+			if output[1] == blue: #Change this to the colour of you tilt
+				tempf = float(output[2]) #convert the string for the temperature to a float type
+
+				gotData = 1
+				tiltSG = float(output[3])
+				tiltTemp = tempf
+
+	#assign values to a dictionary variable for the http POST to google sheet
+	data= 	{
+			"Time": datetime.now(),
+			'SG': tiltSG,
+			'Temp': tiltTemp,
+			}
+	blescan.hci_disable_le_scan(sock)
+	return data
+
+
+def main():
+
+		data = getdata()
+		#print "SG/Concentration: ",data["SG"]," Temperature: ",(data["Temp"]-32)*5/9,"C"
+		#print "at ", str(data["Time"])
+		tempList = list(str((data["Temp"]-32)*5/9))
+		isSingleDigit = True if tempList.index(".") == 1 else False
+		tempDigits = tempList[:tempList.index(".")] + tempList[tempList.index(".")+1:]
+		gravity = list(str(data["SG"]))
+
+		if data["SG"] < 1000:
+			gravity.insert(0,"0")
+
+		# To show temperature
+		device.letter(1, 8, tempDigits[0], isSingleDigit)
+		device.letter(1, 7, tempDigits[1], not isSingleDigit)
+		device.letter(1, 6, tempDigits[2])
+		device.letter(1, 5, "deg")
+
+		# To show time
+		# now = data["Time"]
+		# hour = now.hour
+		# minute = now.minute
+		# second = now.second
+		# dot = second % 2 == 0 # calculate blinking dot
+		# Set hours
+		# device.letter(1, 8, int(hour / 10)) # Tens
+		# device.letter(1, 7, hour % 10, True) # Ones
+		# Set minutes
+		# device.letter(1, 6, int(minute / 10)) # Tens
+		# device.letter(1, 5, minute % 10) # Ones 
+
+
+		# To show gravity
+		device.letter(1, 4, gravity[0], True)
+		device.letter(1, 3, gravity[1])
+		device.letter(1, 2, gravity[2])
+		device.letter(1, 1, gravity[3])
+
+if __name__ == "__main__": #dont run this as a module
+	main()
